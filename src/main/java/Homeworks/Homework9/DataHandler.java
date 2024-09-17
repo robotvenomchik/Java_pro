7124548e9ef29ea20acaf168beb9b111c917c3e0
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2024-09-17 22:17:46 +0300

    Homework9

---
+++ src/main/java/Homeworks/Homework9/DataHandler.java
@@ -0,0 +1,15 @@
+package Homeworks.Homework9;
+
+public class DataHandler {
+
+    public <T> String handleData(T[] items) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (T item : items) {
+            count++;
+            sb.append(String.format("(%d) %s ", count, item.toString()));
+        }
+        return sb.toString();
+    }
+}
+
